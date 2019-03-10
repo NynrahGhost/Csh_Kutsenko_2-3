6f3a4a8979359a601de6322bfb9c0442b6a2a779
Ghost <xxyyzzghost@gmail.com>
2019-03-10 12:36:15 +0200

    Init Commit

---
+++ Views/UserControls/BirthDateUserControl.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Csh_Kutsenko_01.Tools.Managers;
+using Csh_Kutsenko_01.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Csh_Kutsenko_01.Views.UserControls
+{
+    /// <summary>
+    /// Interaction logic for BirthDateUserControl.xaml
+    /// </summary>
+    public partial class BirthDateUserControl : UserControl, INavigatable
+    {
+        public BirthDateUserControl()
+        {
+            InitializeComponent();
+            DataContext = new BirthDateViewModel();
+        }
+    }
+}
